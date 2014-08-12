2b356c42ed911ef2fb9b98a7024bcd25e96aff65
nook <nook@harquail.com>
2014-08-12 16:05:22 -0400

    started

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php //next eample will change status of specific conversation to resolve
+$service_url = 'http://example.com/api/conversations/cid123/status';
+$ch = curl_init($service_url);
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+$data = array("status" => 'R');
+curl_setopt($ch, CURLOPT_POSTFIELDS,http_build_query($data));
+$response = curl_exec($ch);
+if ($response === false) {
+    $info = curl_getinfo($ch);
+    curl_close($ch);
+    die('error occured during curl exec. Additioanl info: ' . var_export($info));
+}
+curl_close($ch);
+$decoded = json_decode($response);
+if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') {
+    die('error occured: ' . $decoded->response->errormessage);
+}
+echo 'response ok!';
+var_export($decoded->response);?>
