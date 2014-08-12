d84af5e776b0dd81aa6d26c92a2f332b0301df7b
nook <nook@harquail.com>
2014-08-12 17:03:20 -0400

    switched to nodejs

--- web.js
+++ web.js
@@ -1,8 +1,38 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
+var http = require('http'); //the variable doesn't necessarily have to be named http
 
 app.use(logfmt.requestLogger());
+
+
+var options = {
+  host: '129.170.212.42',
+  port: 80,
+  path: '/api/newdeveloper/groups/0/action',
+  method: 'PUT'
+};
+
+var req = http.request(options, function(res) {
+  console.log('STATUS: ' + res.statusCode);
+  console.log('HEADERS: ' + JSON.stringify(res.headers));
+  res.setEncoding('utf8');
+  res.on('data', function (chunk) {
+    console.log('BODY: ' + chunk);
+  });
+});
+
+req.on('error', function(e) {
+  console.log('problem with request: ' + e.message);
+});
+
+var data = {
+  on: true,
+};
+// write data to request body
+req.write(JSON.stringify(data));
+req.end();
+
 
 app.get('/', function(req, res) {
   res.send('Hello World!');
