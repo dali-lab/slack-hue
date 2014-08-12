510a2b06725b384778ef6937366b068042d49a36
nook <nook@harquail.com>
2014-08-12 16:22:05 -0400

    rawr

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $ch = curl_init($service_url);
 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
-$data = array(false => "on");
+$data = array("on" => false);
 curl_setopt($ch, CURLOPT_POSTFIELDS,http_build_query($data));
 $response = curl_exec($ch);
 if ($response === false) {
