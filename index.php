63ed374a16b67d47dd4bcd1d0a1277ad783e494a
nook <nook@harquail.com>
2014-08-12 16:24:52 -0400

    rawr

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 $service_url = 'http://129.170.212.42/api/newdeveloper/groups/0/action';
 $ch = curl_init($service_url);
-
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
 curl_setopt($ch, CURLOPT_VERBOSE, 1);
@@ -9,7 +8,7 @@ $data = array("on" => false);
 curl_setopt($ch, CURLOPT_POSTFIELDS,http_build_query($data));
 $response = curl_exec($ch);
 if ($response === false) {
-	echo 'no response!';	
+		
     $info = curl_getinfo($ch);
     curl_close($ch);
     die('error occured during curl exec. Additional info: ' . var_export($info));
@@ -20,4 +19,5 @@ if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') 
     die('error occured: ' . $decoded->response->errormessage);
 }
 echo 'response ok!';
+echo "$ch";
 var_export($decoded->response);?>
