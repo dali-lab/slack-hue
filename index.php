cd236c14d960bba9fe56588438b921045b91480d
nook <nook@harquail.com>
2014-08-12 16:29:47 -0400

    rawr

--- index.php
+++ index.php
@@ -19,6 +19,4 @@ if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') 
     die('error occured: ' . $decoded->response->errormessage);
 }
 echo 'response ok!';
-echo $data;
-echo "$ch";
 var_export($decoded->response);?>
