e738509a36c709a6a4110bdd04dbb0ccf7a8b4c9
Nook <nook@harquail.com>
2015-02-27 00:19:34 -0500

    change lights in sections of room

--- web.js
+++ web.js
@@ -1,5 +1,5 @@
 // Nook Harquail
-// 
+//
 
 var express = require("express");
 var logfmt = require("logfmt");
@@ -15,90 +15,98 @@ var NUM_LIGHTS = 15;
 
 //the ip address of the hue basestation.  dalights.cs.dartmouth.edu
 var options = {
-  host: '129.170.212.42',
-  port: 80,
-  path: '/api/newdeveloper/groups/0/action',
-  method: 'PUT'
+host: '129.170.212.42',
+port: 80,
+path: '/api/newdeveloper/groups/0/action',
+method: 'PUT'
 };
 
 var shouldTV = false;
 
+//range of lights near oscar
+var oscarLights = [10,12,13,15,3,1];
+//range of lights near tv
+var tvLights = [9,11,6,5,4,2,0,7,8];
+//range of lights in center
+var centerLights = [14];
+
+
 var onData = {
-  on: true,
+on: true,
 };
 
 var offData = {
-  on: false,
+on: false,
 };
 
 var onData = {
-  on: true,
+on: true,
 };
 
 var partyData = {
-  on: true,
-  effect: 'colorloop',
+on: true,
+effect: 'colorloop',
 };
 
 var veryBrightData = {
-  on: true,
-  bri: 255,
+on: true,
+bri: 255,
 };
 
 var brightData = {
-  on: true,
-  bri: 230,
+on: true,
+bri: 230,
 };
 var mediumBrightData = {
-  on: true,
-  bri: 200,
-  
+on: true,
+bri: 200,
+    
 };
 
 var lessDimData = {
-  on: true,
-  bri: 155,
-  
+on: true,
+bri: 155,
+    
 };
 
 var dimData = {
-  on: true,
-  bri: 100,
+on: true,
+bri: 100,
 };
 
 var redData = {
-  on: true,
-  hue:0,
-  sat: 255,
-  effect: 'none'
+on: true,
+hue:0,
+sat: 255,
+effect: 'none'
 };
 
 var orangeData = {
-  on: true,
-  hue:13000,
-  sat: 255,
-  effect: 'none',
+on: true,
+hue:13000,
+sat: 255,
+effect: 'none',
 };
 var greenData = {
-  on: true,
-  hue:25500,
-  sat: 255,
-  effect: 'none',
+on: true,
+hue:25500,
+sat: 255,
+effect: 'none',
 };
 
 var pulseData = {
-  alert: 'select'
+alert: 'select'
 };
 
 var pulseManyData = {
-  alert: 'lselect'
+alert: 'lselect'
 };
 
 var normalData = {
-	  on: true,
-	  hue:15000,
-	  sat: 200,
-	  effect: 'none',
+on: true,
+hue:15000,
+sat: 200,
+effect: 'none',
 }
 
 function getRandomArbitrary(min, max) {
@@ -106,187 +114,258 @@ function getRandomArbitrary(min, max) {
 }
 
 var blueData = {
-    on: true,
-	hue: 46920,
-	sat: 255,
+on: true,
+hue: 46920,
+sat: 255,
 };
 
 var purpleData = {
-    on: true,
-	hue: 50000,
-	sat: 255,
+on: true,
+hue: 50000,
+sat: 255,
 };
 // request.write(JSON.stringify(data));
 // request.end();
 
 function intForColor(color){
-
+    
 }
 
 function lightWithDataAndNumber(data,number){
-	
-	var options2 = {
-	  host: '129.170.212.42',
-	  port: 80,
-	  path: '/api/newdeveloper/lights/'+number+'/state',
-	  method: 'PUT'
-	};
-	
-	var request = http.request(options2, function(res) {
-	  console.log('STATUS: ' + res.statusCode);
-	  console.log('HEADERS: ' + JSON.stringify(res.headers));
-	  res.setEncoding('utf8');
-	  res.on('data', function (chunk) {
-	    console.log('BODY: ' + chunk);
-	  });
-	});
-	request.on('error', function(e) {
-	  console.log('problem with request: ' + e.message);
-	});
-	request.write(JSON.stringify(data));
-	request.end();	
+    
+    var options2 = {
+    host: '129.170.212.42',
+    port: 80,
+    path: '/api/newdeveloper/lights/'+number+'/state',
+    method: 'PUT'
+    };
+    
+    var request = http.request(options2, function(res) {
+                               console.log('STATUS: ' + res.statusCode);
+                               console.log('HEADERS: ' + JSON.stringify(res.headers));
+                               res.setEncoding('utf8');
+                               res.on('data', function (chunk) {
+                                      console.log('BODY: ' + chunk);
+                                      });
+                               });
+    request.on('error', function(e) {
+               console.log('problem with request: ' + e.message);
+               });
+    request.write(JSON.stringify(data));
+    request.end();
 }
 
 
 function lightsWithData(data){
-	var request = http.request(options, function(res) {
-	  console.log('STATUS: ' + res.statusCode);
-	  console.log('HEADERS: ' + JSON.stringify(res.headers));
-	  res.setEncoding('utf8');
-	  res.on('data', function (chunk) {
-	    console.log('BODY: ' + chunk);
-	  });
-	});
-	request.on('error', function(e) {
-	  console.log('problem with request: ' + e.message);
-	});
-	request.write(JSON.stringify(data));
-	request.end();
+    var request = http.request(options, function(res) {
+                               console.log('STATUS: ' + res.statusCode);
+                               console.log('HEADERS: ' + JSON.stringify(res.headers));
+                               res.setEncoding('utf8');
+                               res.on('data', function (chunk) {
+                                      console.log('BODY: ' + chunk);
+                                      });
+                               });
+    request.on('error', function(e) {
+               console.log('problem with request: ' + e.message);
+               });
+    request.write(JSON.stringify(data));
+    request.end();
 }
 
 app.post('/', function(req,res){
-	
+         
          console.log('body: '+req.body);
          console.log('text :'+req.body.text);
          
-	if (text != 'tv'){
-		shouldTV = false;
-	}
-	
-	var text = req.body.text;
-	if( text =='on') {		
-		lightsWithData(onData);
-		res.send('lights on');	
-	}
-	else if (text == 'tv'){
-		shouldTV = true;
-		res.send('tv');	
-	}
-	else if (text == 'off'){
-		lightsWithData(offData);
-		res.send('lights off');	
-	}
-	else if (text == 'party'){
-		lightsWithData(partyData);
-		res.send('party!');	
-	}
-	else if (text == 'dim'){
-		lightsWithData(lessDimData);
-		res.send('dimmed');	
-	}
-	else if (text == 'very dim'){
-		lightsWithData(dimData);
-		res.send('dimmed ;)');	
-	}
-	else if (text == 'shady'){
-		lightsWithData(mediumBrightData);
-		res.send('shady');	
-	}
-	else if (text == 'shady'){
-		lightsWithData(mediumBrightData);
-		res.send('shady');	
-	}
-	else if (text == 'bright'){
-		lightsWithData(brightData);
-		res.send('bightened');	
-	}
-	else if (text == 'very bright'){
-		lightsWithData(veryBrightData);
-		res.send('bightened!!');	
-	}
-	else if (text == 'sauron'){
-		lightsWithData(redData);
-		res.send('sauron');	
-	}
-	else if (text == 'blue'){
-		lightsWithData(blueData);
-		res.send('blue');	
-	}
-	else if (text == 'orange'){
-		lightsWithData(orangeData);
-		res.send('orange');	
-	}
-	else if (text == 'green'){
-		lightsWithData(greenData);
-		res.send('green');	
-	}
-	else if (text == 'normal'){
-		lightsWithData(normalData);
-		res.send('normal');	
-	}
-	else if (text == 'purple'){
-		lightsWithData(purpleData);
-		res.send('purple');	
-	}
-	else if (text == 'pulse'){
-		lightsWithData(pulseData);
-		res.send('pulse');	
-	}
-	else if (text == 'pulses'){
-		lightsWithData(pulseManyData);
-		res.send('pulses');	
-	}
-	else if (text == 'random'){
-		
-		for(i =0; i<NUM_LIGHTS; i++){	
-			var tempData = {
-				on: true,
-				hue: getRandomArbitrary(0,65535),
-				sat: getRandomArbitrary(130,255)
-			};
-			console.log(tempData);
-		lightWithDataAndNumber(tempData	,i);
-		}
-		res.send('~*$random!@#~')
-	}
-	else if (text == 'colors'){
-		
-		for(i =0; i<NUM_LIGHTS; i++){	
-			var tempData = {
-				on: true,
-				hue: getRandomArbitrary(0,65535),
-				sat: getRandomArbitrary(220,255)
-			};
-			console.log(tempData);
-		lightWithDataAndNumber(tempData	,i);
-		}
-		res.send('~*$random colors!@#~')
-	}
-	else{
-		res.send('/lights commands: on, off, random, very dim, dim, shady, bright, very bright, blue, green, orange, purple, normal, colors, sauron, party, pulse, pulses tv');	
-	}
-	
-});
+         
+         var texts = req.body.text.split(" ");
+         
+         console.log("LENGTH: "+ texts.length);
+         
+         //defines color
+         var text = texts[0];
+         console.log("COLOR:"+ text);
+         //defines lights controlled
+         var text2 = texts[1];
+         console.log("text2:"+ text2);
+         var lightsControlled;
+         switch(text2){
+            case "oscar": lightsControlled = oscarLights;   break;
+         case "tv": lightsControlled = tvLights; break;
+         case "table": lightsControlled = centerLights; break;
+            default: lightsControlled = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+            break;
+         
+         }
+         
+         
+         if (text != 'tv'){
+         shouldTV = false;
+         }
+         
+         if( text =='on') {
+         
+         for(var i =0; i<lightsControlled.length; i++){
+         lightWithDataAndNumber(onData,lightsControlled[i]);
+         }
+         
+         res.send('lights on');
+         }
+         else if (text == 'tv'){
+         shouldTV = true;
+         res.send('tv');
+         }
+         else if (text == 'off'){
+         for(var i =0; i<lightsControlled.length; i++){
+         lightWithDataAndNumber(offData,lightsControlled[i]);
+         }
+         res.send('lights off');
+         }
+         else if (text == 'party'){
+         for(var i =0; i<lightsControlled.length; i++){
+         lightWithDataAndNumber(partyData,lightsControlled[i]);
+         }
+         res.send('party!');
+         }
+         else if (text == 'dim'){
+         for(var i =0; i<lightsControlled.length; i++){
+         lightWithDataAndNumber(lessDimData,lightsControlled[i]);
+         }
+         res.send('dimmed');
+         }
+         else if (text == 'very dim'){
+         for(var i =0; i<lightsControlled.length; i++){
+         lightWithDataAndNumber(dimData,lightsControlled[i]);
+         }
+         res.send('dimmed ;)');
+         }
+         else if (text == 'shady'){
+         
+         for(var i =0; i<lightsControlled.length; i++){
+         lightWithDataAndNumber(mediumBrightData,lightsControlled[i]);
+         }
+         res.send('shady');
+         }
+         else if (text == 'bright'){
+         
+         for(var i =0; i<lightsControlled.length; i++){
+         lightWithDataAndNumber(brightData,lightsControlled[i]);
+         }
+         res.send('bightened');
+         }
+         else if (text == 'very bright'){
+         for(var i =0; i<lightsControlled.length; i++){
+         lightWithDataAndNumber(veryBrightData,lightsControlled[i]);
+         }
+         res.send('bightened!!');
+         }
+         else if (text == 'sauron'){
+         console.log("SAURON");
+         for(var i =0; i<lightsControlled.length; i++){
+         console.log("\n\nTHIS IS I:\n"+lightsControlled[i]);
+         lightWithDataAndNumber(redData,lightsControlled[i]);
+         }
+         res.send('sauron');
+         }
+         else if (text == 'blue'){
+         
+         for(var i =0; i<lightsControlled.length; i++){
+         console.log("\n\nTHIS IS I:\n"+lightsControlled[i]);
+         lightWithDataAndNumber(blueData,lightsControlled[i]);
+         }
+         
+         res.send('blue');
+         }
+         else if (text == 'orange'){
+         for(var i =0; i<lightsControlled.length; i++){
+         console.log("\n\nTHIS IS I:\n"+lightsControlled[i]);
+         lightWithDataAndNumber(orangeData,lightsControlled[i]);
+         }
+         res.send('orange');
+         }
+         else if (text == 'green'){
+         
+         for(var i =0; i<lightsControlled.length; i++){
+         console.log("\n\nTHIS IS I:\n"+lightsControlled[i]);
+         lightWithDataAndNumber(greenData,lightsControlled[i]);
+         }
+         res.send('green');
+
+         }
+         else if (text == 'normal'){
+         
+         for(var i =0; i<lightsControlled.length; i++){
+            console.log("\n\nTHIS IS I:\n"+lightsControlled[i]);
+            lightWithDataAndNumber(normalData,lightsControlled[i]);
+         }
+         res.send('normal');
+         }
+         else if (text == 'purple'){
+         
+         for(var i =0; i<lightsControlled.length; i++){
+         console.log("\n\nTHIS IS I:\n"+lightsControlled[i]);
+         lightWithDataAndNumber(purpleData,lightsControlled[i]);
+         }
+         res.send('purple');
+         }
+         else if (text == 'pulse'){
+         lightsWithData(pulseData);
+         res.send('pulse');
+         }
+         else if (text == 'pulses'){
+         
+         lightsWithData(pulseManyData);
+         
+         res.send('pulses');
+         }
+         else if (text == 'sparkle'){
+         
+         for(i in lightsControlled){
+         lightWithDataAndNumber(pulseManyData,i);
+         }
+         res.send('sparkle');
+         }
+         else if (text == 'random'){
+         
+         for(var i =0; i<lightsControlled.length; i++){
+         var tempData = {
+         on: true,
+         hue: getRandomArbitrary(0,65535),
+         sat: getRandomArbitrary(130,255)
+         };
+         console.log(tempData);
+         lightWithDataAndNumber(tempData, lightsControlled[i]);
+         }
+         res.send('~*$random!@#~')
+         }
+         else if (text == 'colors'){
+         
+         for(var i =0; i<lightsControlled.length; i++){
+         var tempData = {
+         on: true,
+         hue: getRandomArbitrary(0,65535),
+         sat: getRandomArbitrary(220,255)
+         };
+         console.log(tempData);
+         lightWithDataAndNumber(tempData, lightsControlled[i]);
+         }
+         res.send('~*$random colors!@#~')
+         }
+         else{
+         res.send('/lights commands: on, off, random, very dim, dim, shady, bright, very bright, blue, green, orange, purple, normal, colors, sauron, party, pulse, pulses tv');
+         }
+         
+         });
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
+        res.send('Hello World!');
+        });
 
 app.get('/tv/', function(req, res) {
-  res.send(shouldTV);
-});
+        res.send(shouldTV);
+        });
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+           console.log("Listening on " + port);
+           });
