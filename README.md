fe697da8acb981652a32fdf85a883db857924049
Tim Tregubov <tim@zingweb.com>
2017-01-22 21:19:09 -0500

    refactor for new system

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 slack-hue
 =========
 Run using the /lights command in slack
+
+to Debug:
+
+```
+ curl -X POST -H "Content-Type: application/json" -d '{"text": "normal blue"}' "http://localhost:5000/"
+ ```
--- src/server.js
+++ src/server.js
@@ -3,7 +3,11 @@ import logfmt from 'logfmt';
 import bodyParser from 'body-parser';
 import hue from 'node-hue-api';
 import tinycolor from 'tinycolor2';
+import colornames from 'colornames';
+import _ from 'lodash';
+import dotenv from 'dotenv';
 
+dotenv.config({ silent: true });
 
 const app = express();
 app.use(bodyParser.json()); // to support JSON-encoded bodies
@@ -13,10 +17,11 @@ app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
 app.use(logfmt.requestLogger());
 
 
-const HueApi = hue.HueApi;
-const lightState = hue.lightState;
+const hostnames = process.env.HOSTNAMES.split(',');
+const usernames = process.env.USERNAMES.split(',');
 
-const nametoHex = require('colornames');
+const apis = hostnames.map((host, i) => { return new hue.HueApi(hostnames[i], usernames[i]); });
+const lightState = hue.lightState;
 
 const displayResult = (result) => {
   console.log(JSON.stringify(result, null, 2));
@@ -26,190 +31,82 @@ const displayError = (err) => {
   console.log(err);
 };
 
-const hostname = '129.170.212.42';
-const username = 'ececaeb13474f4f324487f375198d7';
-const api = new HueApi(hostname, username);
 
-// group ids
-const oscarLights = 2;
-const centerLights = 3;
-const tvLights = 4;
-const locations = ['oscar', 'center', 'tv'];
+// api.config().then(displayResult).done();
+// api.fullState().then(displayResult).done();
 
+const config = {};
 
-function getRandomArbitrary(min, max) {
-  return Math.round(Math.random() * ((max - min) + min));
+async function fetchLightGroups(api) {
+  return api.scenes().then((res) => {
+    return res.slice(1)
+    .map((g) => { return { name: g.name.toLowerCase(), id: g.id, owner: g.owner, lights: g.lights.sort().join() }; });
+  }).then((scenes) => {
+    return api.groups().then((res) => {
+      res.slice(1).forEach((g) => {
+        config[g.name] = { api, id: g.id, lights: g.lights.sort().join(), owner: g.owner };
+        const matches = scenes.filter((a) => { return a.lights === g.lights.sort().join(); });
+        config[g.name].scenes = matches.map((x) => { return { name: x.name.toLowerCase(), id: x.id, owner: x.owner }; });
+      });
+    });
+  });
 }
 
+// get configs for all the apis (every hue controller)
+apis.forEach((api) => { fetchLightGroups(api); });
 
-const normalState = lightState.create().on().white(325, 100);
-const onState = lightState.create().on();
-const offState = lightState.create().off();
-const partyState = lightState.create().on().colorLoop();
-const dimmestState = lightState.create().bri(50);
-const dimState = lightState.create().bri(100);
-const shadyState = lightState.create().bri(150);
-const brightState = lightState.create().bri(200);
-const brightestState = lightState.create().bri(250);
-
-const flashState = lightState.create().alertShort();
-const pulsesState = lightState.create().alertLong();
-
-const states = ['normal', 'on', 'off', 'party', 'dimmest', 'dim', 'shady',
-'bright', 'brightest', 'flash', 'pulses'];
+const extraStates = {
+  party: lightState.create().on().colorLoop(),
+  flash: lightState.create().alertShort(),
+  pulse: lightState.create().alertLong(),
+};
 
 
+// handle posts in the format of: location scene.   set up as outgoing webhook in slack
 app.post('/', (req, res) => {
-  console.log(`body: ${req.body}`);
-  console.log(`text :${req.body.text}`);
+  const message = req.body.text.split(' ');
 
+  const location = message[0];
+  console.log(`location: ${location}`);
 
-  const texts = req.body.text.split(' ');
+  const scene = message.slice(1).join(' ');
+  console.log(`scene: ${scene}`);
 
-  console.log(`LENGTH: ${texts.length}`);
+  const hex = colornames(scene);
+  console.log(`hex: ${hex}`);
 
-  // defines color
-  const text = texts[0];
-  const hexFromText = nametoHex(text);
-  console.log(`COLOR:${text}`);
-  // defines lights controlled
-  const text2 = texts[1];
-  console.log(`text2:${text2}`);
-  let lightsControlled;
-  switch (text2) {
-    case 'oscar':
-      lightsControlled = oscarLights;
-      break;
-    case 'tv':
-      lightsControlled = tvLights;
-      break;
-    case 'table':
-      lightsControlled = centerLights;
-      break;
-    default:
-      lightsControlled = 0;
-      break;
+  const simpleconfig = {}; // for print
+  Object.entries(config).forEach(([k, v]) => { simpleconfig[k] = _.sortedUniq(v.scenes.map((s) => { return s.name.toLowerCase(); }).sort()); });
 
+  // check if scene to active is a possible option
+  const sceneToActivate = (location in config) ? config[location].scenes.filter((s) => { return s.name === scene; })[0] : null;
+
+  if (location in config && sceneToActivate) {
+    console.log(`location ${location} and scene ${sceneToActivate.name} verified`);
+    config[location].api.activateScene(sceneToActivate.id)
+      .then(displayResult)
+      .fail(displayError)
+      .done(res.send('Can do!'));
+  } else if (location && scene in extraStates) {
+    config[location].api.setGroupLightState(config[location].id, extraStates[scene])
+      .then(displayResult)
+      .fail(displayError)
+      .done(res.send('Can do!'));
+  } else if (location && hex) {
+    // if there is a converted hex color then add some saturation and set that color
+    const color = tinycolor(hex).saturate(98);
+    console.log(color);
+    const hslColor = color.toHsl();
+    console.log(hslColor);
+    const namedColorState = lightState.create().on().hsl(hslColor.h, hslColor.s * 100, hslColor.l * 100);
+    config[location].api.setGroupLightState(config[location].id, namedColorState)
+      .then(displayResult)
+      .fail(displayError)
+      .done(res.send('Can do!'));
+  } else {
+    console.log('unknown location or scene');
+    res.send(`/lights [location] [scene||colorname||party]\n${JSON.stringify(simpleconfig).replace(/"|{|}/g, '').replace(/],/g, ']\n')}`);
   }
-
-  const intFromText = parseInt(text, 10);
-
-
-  // if (text === 'on') {
-  //   api.setGroupLightState(lightsControlled, onState)
-  //     .then(displayResult)
-  //     .fail(displayError)
-  //     .done();
-  //
-  //   res.send('lights on');
-  // } else if (text === 'tv') {
-  //   res.send('tv');
-  // } else if (text === 'off') {
-  //   api.setGroupLightState(lightsControlled, offState) // provide a value of false to turn off
-  //   .then(displayResult)
-  //     .fail(displayError)
-  //     .done();
-  //   res.send('lights off');
-  // } else if (text === 'party') {
-  //   api.setGroupLightState(lightsControlled, partyState) // provide a value of false to turn off
-  //   .then(displayResult)
-  //     .fail(displayError)
-  //     .done();
-  //   res.send('party!');
-  // } else if (text === 'dim') {
-  //   api.setGroupLightState(lightsControlled, dimState) // provide a value of false to turn off
-  //   .then(displayResult)
-  //     .fail(displayError)
-  //     .done();
-  //   res.send('dim ;)');
-  // } else if (text === 'dimmest') {
-  //   api.setGroupLightState(lightsControlled, dimmestState) // provide a value of false to turn off
-  //   .then(displayResult)
-  //     .fail(displayError)
-  //     .done();
-  //   res.send('dimmed ;)');
-  // } else if (text === 'shady') {
-  //   api.setGroupLightState(lightsControlled, shadyState) // provide a value of false to turn off
-  //   .then(displayResult)
-  //     .fail(displayError)
-  //     .done();
-  //   res.send('shady');
-  // } else if (text === 'bright') {
-  //   api.setGroupLightState(lightsControlled, brightState) // provide a value of false to turn off
-  //   .then(displayResult)
-  //     .fail(displayError)
-  //     .done();
-  //   res.send('bightened');
-  // } else if (text === 'brightest') {
-  //   api.setGroupLightState(lightsControlled, brightestState) // provide a value of false to turn off
-  //   .then(displayResult)
-  //     .fail(displayError)
-  //     .done();
-  //   res.send('bightened!!');
-  // } else if (!isNaN(intFromText)) {
-  //   const whiteState = lightState.create().on().white(intFromText, 100);
-  //
-  //   api.setGroupLightState(lightsControlled, whiteState) // provide a value of false to turn off
-  //   .then(displayResult)
-  //     .fail(displayError)
-  //     .done();
-  //   res.send(`white point set (154 - 500): ${intFromText}`);
-  // } else if (text === 'normal') {
-  //   api.setGroupLightState(lightsControlled, normalState) // provide a value of false to turn off
-  //   .then(displayResult)
-  //     .fail(displayError)
-  //     .done();
-  //   res.send('normal');
-  // } else if (text === 'flash') {
-  //   api.setGroupLightState(lightsControlled, flashState) // provide a value of false to turn off
-  //   .then(displayResult)
-  //     .fail(displayError)
-  //     .done();
-  //   res.send('pulse');
-  // } else if (text === 'pulses') {
-  //   api.setGroupLightState(lightsControlled, pulsesState) // provide a value of false to turn off
-  //   .then(displayResult)
-  //     .fail(displayError)
-  //     .done();
-  //   res.send('pulses');
-  // } else if (text === 'random') {
-  //   for (let i = 0; i < 25; i++) {
-  //     const randomColorState = lightState.create().on().rgb(getRandomArbitrary(0, 255), getRandomArbitrary(0, 255), getRandomArbitrary(0, 255));
-  //
-  //     api.setLightState(i, randomColorState) // provide a value of false to turn off
-  //     .then(displayResult)
-  //       .fail(displayError)
-  //       .done();
-  //   }
-  //   res.send('~*$random!@#~');
-  // } else if (text === 'colors') {
-  //   for (let i = 0; i < 25; i++) {
-  //     const randomColorState = lightState.create().on().hsl(getRandomArbitrary(0, 359), 100, 100);
-  //
-  //     api.setLightState(i, randomColorState) // provide a value of false to turn off
-  //     .then(displayResult)
-  //       .fail(displayError)
-  //       .done();
-  //   }
-  //   res.send('~*$random colors!@#~');
-  // } else if (hexFromText !== undefined) {
-  //   const color = tinycolor(hexFromText);
-  //
-  //   const hslColor = color.toHSL();
-  //   console.log(hslColor);
-  //   const namedColorState = lightState.create().on().hsl(hslColor.h, hslColor.s, 90);
-  //
-  //   api.setGroupLightState(lightsControlled, namedColorState) // provide a value of false to turn off
-  //   .then(displayResult)
-  //     .fail(displayError)
-  //     .done();
-  //
-  //   res.send(`set to hex value: ${hexFromText}for color: ${text}`);
-  // } else {
- // help
-
-  res.send(`/lights [command] [location (optional)]:\nCommands: ${states.join(', ')}\nLocations: ${locations.join(', ')}`);
-  // }
 });
 
 app.get('/', (req, res) => {
